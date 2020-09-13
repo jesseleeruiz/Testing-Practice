53f38692fb5b2bf694f75155f91fbdb6be3455b3
jesseleeruiz <jesseleeruiz@gmail.com>
2020-09-13 09:17:46 -1000

    Added Hater Struct.

---
+++ First Test/First Test/Hater.swift
@@ -0,0 +1,23 @@
+//
+//  Hater.swift
+//  First Test
+//
+//  Created by Jesse Ruiz on 9/13/20.
+//  Copyright © 2020 Jesse Ruiz. All rights reserved.
+//
+
+import Foundation
+
+struct Hater {
+    
+    var hating = false
+    
+    mutating func hadABadDay() {
+        hating = true
+    }
+        
+    mutating func hadAGoodDay() {
+        hating = false
+    }
+    
+}
