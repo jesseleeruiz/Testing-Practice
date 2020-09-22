3d9402a7840c4e6802ac5d4d88b2f74bf21e2438
jesseleeruiz <jesseleeruiz@gmail.com>
2020-09-22 09:51:43 -1000

    Finished practicing Asynchronous Testing.

---
+++ First Test/AsynchronousTests/AsynchronousTests.swift
@@ -0,0 +1,66 @@
+//
+//  AsynchronousTests.swift
+//  AsynchronousTests
+//
+//  Created by Jesse Ruiz on 9/15/20.
+//  Copyright © 2020 Jesse Ruiz. All rights reserved.
+//
+
+import XCTest
+@testable import First_Test
+
+class AsynchronousTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+    func DISABLED_testPtimesUpTo100ShouldBe25() {
+        // Given
+        let maximumCount = 100
+        let expectation = XCTestExpectation(description: "Calculate primes up to \(maximumCount)")
+        
+        // When
+        PrimeCalculator.calculate(upTo: maximumCount) {
+            // Then
+            XCTAssertEqual($0.count, 25)
+            // That sets a maximum value of 100, then asks the prime
+            // calculator to figure out all the primes up to that number.
+            // Finally, it asserts that zero primes were found.
+            expectation.fulfill()
+        }
+        wait(for: [expectation], timeout: 10)
+    }
+    
+    func testPrimesUpTo100ShouldBe25() {
+        // Given
+        let maxCount = 100
+        
+        // When
+        let progress: () = PrimeCalculator.calculate(upTo: maxCount) {
+            XCTAssertEqual($0.count, 25)
+        }
+        
+        // Then
+        let predicate = NSPredicate(format: "%@.completedUnitCount == %@", argumentArray: [progress, maxCount])
+        
+        let expectation = XCTNSPredicateExpectation(predicate: predicate, object: progress)
+        wait(for: [expectation], timeout: 10)
+    }
+}
---
+++ First Test/First Test/PrimeCalculator.swift
@@ -0,0 +1,77 @@
+//
+//  PrimeCalculator.swift
+//  First Test
+//
+//  Created by Jesse Ruiz on 9/15/20.
+//  Copyright © 2020 Jesse Ruiz. All rights reserved.
+//
+
+import Foundation
+
+struct PrimeCalculator {
+//    static func calculate(upTo max: Int, completion: @escaping ([Int]) -> Void) {
+//        // Push our work straight to a background thread
+//        DispatchQueue.global().async {
+//            guard max > 1 else {
+//                // If the input value is 0 or 1 exit immediately
+//                return
+//            }
+//
+//            // Mark all our numbers as prime
+//            var sieve = [Bool](repeating: true, count: max)
+//
+//            // 0 & 1 are by definition not prime
+//            sieve[0] = false
+//            sieve[1] = false
+//
+//            // Count from 0 up to the ceiling
+//            for number in 2..<max {
+//                // If this is marked as prime, then every multiple of it is not prime
+//                if sieve[number] == true {
+//                    for multiple in stride(from: number * number, to: sieve.count, by: number) {
+//                        sieve[multiple] = false
+//                    }
+//                }
+//            }
+//            // Collapse our results down to a single array of primes
+//            let primes = sieve.enumerated().compactMap { $1 == true ? $0 : nil }
+//            completion(primes)
+//        }
+//    }
+    
+    static func calculate(upTo max: Int, completion: @escaping ([Int]) -> Void) -> Progress {
+        // Create a Progress object that counts up to our max number
+        let progress = Progress(totalUnitCount: Int64(max))
+        
+        DispatchQueue.global().async {
+            guard max > 1 else {
+                completion([])
+                return
+            }
+            
+            var sieve = [Bool](repeating: true, count: max)
+            sieve[0] = false
+            sieve[1] = false
+            
+            // Add 2 to our progress counter, because we already went through 0 && 1
+            progress.completedUnitCount += 2
+            
+            for number in 2 ..< max {
+                // Every time we've checked one number, add 1 to our completed unit count
+                progress.completedUnitCount += 1
+                
+                if sieve[number] == true {
+                    for multiple in stride(from: number * number, to: sieve.count, by: number) {
+                        sieve[multiple] = false
+                    }
+                }
+            }
+            let primes = sieve.enumerated().compactMap {
+                $1 == true ? $0 : nil
+            }
+            completion(primes)
+        }
+        // Send back the Progress object
+        return progress
+    }
+}
