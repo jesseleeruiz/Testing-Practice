2ce3c1b3a492975ba6505138f3594fdaed96aea2
jesseleeruiz <jesseleeruiz@gmail.com>
2020-10-21 08:59:15 -1000

    Followed TDD. Started with test that failed, then starting writing production code to fulfill test. Iterated that process until test fully passed. Then looked over code to refactor production code and used a Set instead of an Array for products in User Struct. TDD is awesome

--- TDD/TDD/User.swift
+++ TDD/TDD/User.swift
@@ -6,3 +6,15 @@
 //
 
 import Foundation
+
+struct User {
+    var products = Set<String>()
+    
+    mutating func buy(_ product: String) {
+        products.insert(product)
+    }
+    
+    func owns(_ product: String) -> Bool {
+        return products.contains(product)
+    }
+}
--- TDD/TDDTests/TDDTests.swift
+++ TDD/TDDTests/TDDTests.swift
@@ -18,16 +18,16 @@ class TDDTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testReadingBookAddsToLibrary() {
+        // Given
+        let bookToBuy = "Great Expectations"
+        var user = User()
+        
+        // When
+        user.buy(bookToBuy)
+        
+        // Then
+        XCTAssertTrue(user.owns(bookToBuy))
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
+    
 }
